dbbdf7cadb3bbfa9ac75812c73cfbece3930ddf2
vanthai <nguyenvanthai03102001@gmail.com>
2023-08-30 11:32:57 +0700

    fix todo

--- EX1.js
+++ EX1.js
@@ -6,12 +6,25 @@ const fetchData = async (endpoint) => {
         next(error);
     }
 }
-
+const getPostAndCommentById = async (id) => {
+    try {
+        const [post, comment] = await Promise.all([
+            fetchData(`posts/${id}`),
+            fetchData(`comments/${id}`)
+        ])
+        return {
+            post,
+            comment
+        }
+    } catch (error) {
+        next(error);
+    }
+}
 (async () => {
     try {
         //2. get 10 users
         const getUser = await fetchData('users');
-        console.log(getUser)
+        console.log('all user:', getUser)
 
         //3. Post and Cmt User map
         const [getPost, getComment] = await Promise.all([
@@ -24,8 +37,8 @@ const fetchData = async (endpoint) => {
                 name: user.name,
                 username: user.username,
                 email: user.email,
-                comments: getComment.filter((comment) => comment.email === user.email),
-                posts: getPost.filter((post) => post.userId === user.id)
+                comments: (getComment.filter((comment) => comment.email === user.email)),
+                posts: (getPost.filter((post) => post.userId === user.id))
             }
         });
         console.log("Post comment User map: ", PostCommentUserMap);
@@ -39,40 +52,53 @@ const fetchData = async (endpoint) => {
 
         //5. Reformat the data count post comment
         const reformatUser = UserCommentLengthMore3.map(user => {
+            const { comments, posts, ...another } = user;
             return {
-                id: user.id,
-                name: user.name,
-                username: user.username,
-                email: user.email,
-                comments: user.comments.length,
-                posts: user.posts.length,
+                ...another,
+                commentsCount: comments.length,
+                postsCount: posts.length
             }
         });
         console.log("Reformat: ", reformatUser);
 
         //6. Who is the user with the most comments/posts?
         // reformatUser.slice() //copy
-        const userSortByPost = reformatUser.slice().sort((userA, userB) => {
-            userB.comments - userA.comments
+        // Dung Reduce (accumulator, currentValue)
+        const userMostPost = reformatUser.reduce((userA, userB) => {
+            if (userB.postsCount > userA.postsCount) {
+                return userB;
+            } else {
+                return userA;
+            }
         })
-        console.log('userSortByPost: ', userSortByPost[0]);
+        console.log('userMostPost: ', userMostPost);
 
-        const userSortByCom = reformatUser.slice().sort((userA, userB) => {
-            userB.comments - userA.comments
+        const userMostCom = reformatUser.reduce((userA, userB) => {
+            if (userB.commentsCount > userA.commentsCount) {
+                return userB;
+            } else {
+                return userA;
+            }
         })
-        console.log('userSortByCom: ', userSortByCom[0]);
+        console.log('userMostCom: ', userMostCom);
 
         //7. Sort the list of users by the postsCount value descending?
+        const userSortByPost = reformatUser.slice().sort((userA, userB) => {
+            return userB.postsCount - userA.postsCount;
+        });
         console.log('userSortByPost: ', userSortByPost);
 
         //8. Merge post comment
-        const getPostById1 = getPost.filter(post => post.id === 1);
-        const getCommentByPostId1 = getComment.filter(cmt => cmt.postId === 1)
-        const mergedPostComment = {
-            ...getPostById1,
-            comments: getCommentByPostId1
-        };
-        console.log('Merge: ', mergedPostComment);
+        getPostAndCommentById(1)
+            .then((data) => {
+                const mergedPostComment = {
+                    ...data.post,
+                    comment: [data.comment]
+                };
+                console.log('Merger: ', mergedPostComment)
+            })
+            .catch(err => console.log(err))
+
     } catch (error) {
         console.log(error);
     }
